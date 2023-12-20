98e997c78638ece21c24ed471061d43e792b03b2
cr-booker <cr.booker@outlook.com>
2023-12-19 19:57:06 -0500

    Add icons to player buttons

--- src/components/gametable.jsx
+++ src/components/gametable.jsx
@@ -4,7 +4,7 @@ export default function Table(props){
   return (
     <main>
       <div className="game-table flex-container flex-center">
-        {props.showDealBtn ? <button className="deal-button" onClick={props.loadGame}>Deal</button> :
+        {props.showDealBtn ? <button className="game-table--btn deal-button" onClick={props.loadGame}>Deal</button> :
         <><div className="game-table--dealer flex-container flex-center">
           <div className="game-table--total flex-container flex-center">{props.dealerTotal}</div>
           <div className="game-table--hand flex-container">{props.dealerCards}</div>
@@ -13,9 +13,15 @@ export default function Table(props){
           <div className="game-table--total flex-container flex-center">{props.playerTotal}</div>
           <div className="game-table--hand  flex-container">{props.playerCards}</div>
         </div>
-        <div className="game-table--btn-container flex-container">
-          <button className="player-btn" onClick={props.handleHitBtn}>Hit Me!</button>
-          <button className="player-btn" onClick={props.handleStayBtn}>Stay!</button>
+        <div className="game-table--btn-container flex-container flex-center">
+          <button className="game-table--btn player-btn" onClick={props.handleStayBtn}>
+            <i className="material-icons">pan_tool</i>
+            Stay!
+          </button>
+          <button className="game-table--btn player-btn flex-container flex-center" onClick={props.handleHitBtn}>
+            <i className="material-icons">add_circle</i>
+            Hit Me!
+          </button>
         </div></>}
       </div>
     </main>
